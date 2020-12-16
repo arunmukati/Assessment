08cded5d99a75465faae16c5938ee3c29fb4f9ad
arunmukati <45487420+arunmukati@users.noreply.github.com>
2020-12-16 19:01:50 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { LocalDbService } from './services/local-db.service';
+import { Title,Meta } from '@angular/platform-browser';
 
 @Component({
   selector: "app-root",
@@ -11,6 +12,13 @@ export class AppComponent {
   title = "Assessment";
   constructor(private db: LocalDbService) {
 
+  }
+  ngOnInit{
+    this.metaTagService.addTags([
+				{ property:'og:title' , content : "Rapidken - Fast Learning----" },
+				{ property:'title' , content : "Rapidken - Fast Learning--" },
+				
+			]);
   }
 }
 
